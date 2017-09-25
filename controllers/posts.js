f62fa84cb2949017f5c3461381eef0d542275765
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-09-24 22:16:32 -0500

    Routes for update and delete functional

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,8 +16,32 @@ router.post('/', (req,res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+  Posts.findById(req.params.id, (err, foundPost) => {
+    if(err)
+      res.send(err);
+    // ternary statement
+    (req.body.title) ? foundPost.title = req.body.title : null;
+    (req.body.servings) ? foundPost.servings = req.body.servings : null;
+    (req.body.time) ? foundPost.time = req.body.time : null;
+    (req.body.ingredients) ? foundPost.ingredients = req.body.ingredients : null;
+    (req.body.directions) ? foundPost.directions = req.body.directions : null;
+    (req.body.author) ? foundPost.author = req.body.author : null;
+    foundPost.save((err, updatedPost) => {
+      if(err)
+        res.send(err);
+      else res.json(updatedPost)
+    });
+  });
+});
 
-
+router.delete('/:id', (req, res) => {
+  Posts.findByIdAndRemove(req.params.id, (err, deletedPost) => {
+    if(err)
+      res.send(err);
+    else res.json(deletedPost);
+  });
+});
 
 
 module.exports = router;
